b78816505e6a6ebf13bb27ebc0c818108dec80cd
Akma91 <matejakmacic@gmail.com>
2021-07-28 23:42:38 +0200

    kategorija model i ruta dodani

---
+++ database/migrations/2021_07_28_064608_create_categories_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->timestamps();
+        });
+
+
+        Schema::create('category_movie', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('movie_id');
+
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->foreign('movie_id')->references('id')->on('movies');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
